e7062b79a20bee55e206800ba1f60b9e8c641e78
SteveCutler <stephen.c.cutler@gmail.com>
2023-12-05 18:17:56 -0500

    updated app.js functionality

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { exec } = require('child_process');
+const { spawn } = require('child_process');
 const app = express();
 const port = 3000;
 
@@ -13,24 +13,71 @@ app.post('/evaluate', (req, res) => {
     return res.status(400).json({ error: 'FEN string is required.' });
   }
 
-  // Run Stockfish command to evaluate the position and get the best move
-  exec(`stockfish position fen ${fen} go depth 15`, (error, stdout) => {
-    console.log('Stockfish Output:', stdout); // Add this line for logging
-  
-    if (error) {
-      console.error(error);
-      return res.status(500).json({ error: 'Internal server error.' });
+  // Spawn Stockfish process
+  const stockfish = spawn('stockfish');
+  let stockfishOutput = '';
+
+  // Event handler for Stockfish output
+  stockfish.stdout.on('data', (data) => {
+    stockfishOutput += data.toString();
+
+    // Check if Stockfish has finished analyzing
+    if (stockfishOutput.includes('bestmove')) {
+      const lines = stockfishOutput.split('\n');
+      const bestMoveLine = lines.find((line) => line.includes('bestmove'));
+
+      let evaluation = null;
+
+      // Extract evaluation information from the entire output
+      const evalMatch = stockfishOutput.match(/score (cp|mate) (-?\d+) /);
+      if (evalMatch) {
+        const [, scoreType, scoreValue] = evalMatch;
+        const adjustedValue = (fen.includes('w') ? 1 : -1) * parseInt(scoreValue);
+        const value = (scoreType === 'mate') ? `#${adjustedValue}` : adjustedValue;
+        evaluation = {
+          type: scoreType,
+          value: parseInt(value),
+        };
+      }
+
+      if (bestMoveLine && evaluation !== null) {
+        const matchBestMove = bestMoveLine.match(/^bestmove (\w+)/);
+        const [_, bestMove] = matchBestMove;
+
+        // Close the Stockfish process
+        stockfish.stdin.write('quit\n');
+        stockfish.stdin.end();
+
+        // Return the result in JSON format
+        res.json({
+          bestMove: bestMove || null,
+          evaluation,
+        });
+      } else {
+        // Handle the case where no valid output is received
+        console.error('Invalid Stockfish Output:', stockfishOutput);
+        res.status(500).json({ error: 'Invalid Stockfish output.' });
+
+        // Close the Stockfish process
+        stockfish.stdin.write('quit\n');
+        stockfish.stdin.end();
+      }
     }
-
-    // Parse Stockfish output to extract evaluation and best move
-    const [evaluation, bestMove] = stdout.match(/score (cp|mate) (-?\d+).*?pv (\w+)/);
-
-    // Return the evaluation and best move in JSON format
-    res.json({
-      evaluation: evaluation ? parseFloat(evaluation[2]) : null,
-      bestMove: bestMove || null,
-    });
   });
+
+  // Event handler for errors
+  stockfish.on('error', (error) => {
+    console.error('Stockfish Execution Error:', error);
+    res.status(500).json({ error: 'Internal server error.', details: error.message });
+  });
+
+  // Event handler for closing the Stockfish process
+  stockfish.on('close', (code) => {});
+
+  // Send the UCI commands for analysis
+  stockfish.stdin.write('uci\n');
+  stockfish.stdin.write('isready\n');
+  stockfish.stdin.write(`position fen ${fen}\ngo movetime 1000\n`);
 });
 
 // Handle requests to the root URL
@@ -39,6 +86,6 @@ app.get('/', (req, res) => {
 });
 
 // Start the server
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
+app.listen(port, () => {});
+
+console.log(`Server is running on http://localhost:${port}`);
