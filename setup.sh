9f793c75f29fbf7aae9d70cf3630b205daf6b509
SteveCutler <stephen.c.cutler@gmail.com>
2023-12-09 11:37:40 -0500

    Fix setup script

--- setup.sh
+++ setup.sh
@@ -2,23 +2,24 @@
 
 # Create a directory to store Stockfish in /app
 mkdir -p app/bin
+echo "making bin..."
 
 # Install Stockfish
 echo "Downloading Stockfish..."
-wget https://stockfishchess.org/files/stockfish-linux.zip
+wget https://stockfishchess.org/files/stockfish-linux.zip -O stockfish-linux.zip
 
 echo "Unzipping Stockfish..."
-unzip stockfish-linux.zip
+unzip -q stockfish-linux.zip
 
 echo "Setting execute permissions for Stockfish..."
 chmod +x stockfish
 
 # Move the Stockfish executable to the /app/bin directory
-mv stockfish app/bin/
+mv stockfish* /app/bin/
 
 # Display the new path to Stockfish
 echo "Stockfish installed in: app/bin/stockfish"
 
 # Confirm Stockfish version
 echo "Stockfish version:"
-/app/bin/stockfish -v
+/app/bin/stockfish --version
